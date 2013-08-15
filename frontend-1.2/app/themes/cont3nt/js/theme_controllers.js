1cdb3dbf8f98fa78c4f9788fa4839b6c5f6b335a
npsopeak007 <npsopheak@gmail.com>
2013-08-15 04:05:13 -0700

    ere

--- frontend-1.2/app/themes/cont3nt/js/theme_controllers.js
+++ frontend-1.2/app/themes/cont3nt/js/theme_controllers.js
@@ -164,7 +164,9 @@ $app.controller('HomeController', function ($scope, plus) {
             $('.news-group').html($('.news-group').html() + str);
           }
              
-      b=true;
+         b=true;
+
+          $("#wrapper").niceScroll({touchbehavior:true});
       });
 
       $("#wrapper").niceScroll({touchbehavior:true});
@@ -239,6 +241,35 @@ $app.controller('FreelancerController', function ($scope, plus) {
       alert(0);
     }
     
+
+    $scope.auth={user:"",pass:""};
+    $scope.user={name:""};
+    $scope.login=function(){
+        var dat={};
+        dat['user']=$scope.auth.user;
+        dat['pass']=$scope.auth.pass;
+        $.ajax({
+          type:'POST',
+          data:dat,
+          
+          url:"http://yinkeangseng.byethost8.com/check-user.php"
+          //url:"http://localhost:8030/cont3nt-service/check-user.php"
+        }).done(function(data){
+
+              
+            var objs=eval(data);
+            if(objs.length>0){
+              if(objs[0].status=="fail"){
+                alert("Incorrect Username or password!");
+              }
+              else{
+                alert("Welcome: " + objs[0].username);
+              }
+              $(".popupBox").removeClass("show");
+              $(".popupBox").addClass("hide");
+            }
+        });
+      }
 });
 
 $app.controller('BlogController', function ($scope, plus) {
@@ -250,6 +281,36 @@ $app.controller('BlogController', function ($scope, plus) {
     });
    if(!b)
     $("#wrapper").niceScroll({touchbehavior:true});
+
+
+  $scope.auth={user:"",pass:""};
+    $scope.user={name:""};
+    $scope.login=function(){
+        var dat={};
+        dat['user']=$scope.auth.user;
+        dat['pass']=$scope.auth.pass;
+        $.ajax({
+          type:'POST',
+          data:dat,
+          
+          url:"http://yinkeangseng.byethost8.com/check-user.php"
+          //url:"http://localhost:8030/cont3nt-service/check-user.php"
+        }).done(function(data){
+
+              
+            var objs=eval(data);
+            if(objs.length>0){
+              if(objs[0].status=="fail"){
+                alert("Incorrect Username or password!");
+              }
+              else{
+                alert("Welcome: " + objs[0].username);
+              }
+              $(".popupBox").removeClass("show");
+              $(".popupBox").addClass("hide");
+            }
+        });
+      }
 });
   // defaulting the time on Angular's model variable.
  
